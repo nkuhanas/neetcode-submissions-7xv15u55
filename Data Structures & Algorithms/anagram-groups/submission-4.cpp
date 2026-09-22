70965a59faaba72f07456a4c3759f0713df55233
chace <105261172+nkuhanas@users.noreply.github.com>
2026-09-22 15:18:29 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        unordered_map<string, vector<string>> buckets;
+
+        for (const string& str : strs) {
+
+            string idx = str;
+
+            sort(idx.begin(), idx.end());
+
+            buckets[idx].push_back(str);
+
+        }
+
+        vector<vector<string>> results;
+
+        for (const auto& [_, bucket] : buckets) {
+
+            results.push_back(bucket);
+            
+        }
+
+        return results;
+
+    }
+};
