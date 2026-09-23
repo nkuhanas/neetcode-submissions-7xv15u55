8343659bdc87c5e0c56e35f8cc592e2b9f072792
chace <105261172+nkuhanas@users.noreply.github.com>
2026-09-23 14:51:53 -0700

    Add: trapping-rain-water - submission-14

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-14.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+
+    int trap(vector<int>& height) {
+        
+        int size = static_cast<int>(height.size());
+
+        stack<int> anchors;
+        anchors.push(0);
+
+        int total = 0;
+
+        while (!anchors.empty()) {
+
+            int l = anchors.top();
+            int h = height[l];
+            bool capture = false;
+
+            anchors.pop();
+
+            while (h > 0 && capture == false) {
+
+                int sub = 0;
+                int r = l+1;
+
+                while (r < size && capture == false) {
+
+                    if (height[r] < h) {
+
+                        sub += height[r];
+                        ++r;
+
+                    } else {
+
+                        // we just hit a new anchors
+
+                        anchors.push(r);
+
+                        total += (min(h, height[r]) * (r-l-1) - sub);
+                        capture = true;
+
+                    }
+
+                }
+
+                --h;
+
+            }
+            
+
+            if (capture == false && l < size-1) {
+
+                anchors.push(l+1);
+
+            }
+            
+        }
+
+        return total;
+
+    }
+};
